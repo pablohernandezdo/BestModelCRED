426899dc99f2d312b2d58e357a94fe56d44acf3c
Pablo Hernandez <pablo.hernandezdo.13@sansano.usm.cl>
2021-07-27 04:18:39 -0400

    Train cred models eval iter 30

--- train_cred.sh
+++ train_cred.sh
@@ -1,147 +1,156 @@
 #!/bin/bash
 
-echo "Training model CRED, lr = 1e-2, epochs = 20, batch_size = 256"
+echo "Training model CRED, lr = 1e-2, epochs = 30, batch_size = 256"
 python train.py \
         --lr 1e-2 \
-        --epochs 20 \
+        --device 3 \
+        --epochs 30 \
         --batch_size 256 \
         --earlystop 0 \
-        --eval_iter 1 \
+        --eval_iter 30 \
         --model_folder 'models'  \
         --classifier CRED \
         --model_name CRED_1e2_256 \
         --dataset_name "STEAD-ZEROS" \
-        --train_path "Data/TrainReady/Train_constant.npy" \
-        --val_path "Data/TrainReady/Val_constant.npy" &
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy" &
 
 P1=$!
 
-echo "Training model CRED, lr = 5e-3, epochs = 20, batch_size = 256"
+echo "Training model CRED, lr = 5e-3, epochs = 30, batch_size = 256"
 python train.py \
         --lr 5e-3 \
-        --epochs 20 \
+        --device 3 \
+        --epochs 30 \
         --batch_size 256 \
         --earlystop 0 \
-        --eval_iter 1 \
+        --eval_iter 30 \
         --model_folder 'models'  \
         --classifier CRED \
         --model_name CRED_5e3_256 \
         --dataset_name "STEAD-ZEROS" \
-        --train_path "Data/TrainReady/Train_constant.npy" \
-        --val_path "Data/TrainReady/Val_constant.npy" &
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy" &
 
 P2=$!
 
-echo "Training model CRED, lr = 1e-3, epochs = 20, batch_size = 256"
+echo "Training model CRED, lr = 1e-3, epochs = 30, batch_size = 256"
 python train.py \
         --lr 1e-3 \
-        --epochs 20 \
+        --device 3 \
+        --epochs 30 \
         --batch_size 256 \
         --earlystop 0 \
-        --eval_iter 1 \
+        --eval_iter 30 \
         --model_folder 'models'  \
         --classifier CRED \
         --model_name CRED_1e3_256 \
         --dataset_name "STEAD-ZEROS" \
-        --train_path "Data/TrainReady/Train_constant.npy" \
-        --val_path "Data/TrainReady/Val_constant.npy" &
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy" &
 
 P3=$!
 
-echo "Training model CRED, lr = 5e-4, epochs = 20, batch_size = 256"
+echo "Training model CRED, lr = 5e-4, epochs = 30, batch_size = 256"
 python train.py \
         --lr 5e-4 \
-        --epochs 20 \
+        --device 3 \
+        --epochs 30 \
         --batch_size 256 \
         --earlystop 0 \
-        --eval_iter 1 \
+        --eval_iter 30 \
         --model_folder 'models'  \
         --classifier CRED \
         --model_name CRED_5e4_256 \
         --dataset_name "STEAD-ZEROS" \
-        --train_path "Data/TrainReady/Train_constant.npy" \
-        --val_path "Data/TrainReady/Val_constant.npy" &
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy" &
 
 P4=$!
 
-echo "Training model CRED, lr = 1e-4, epochs = 20, batch_size = 256"
+echo "Training model CRED, lr = 1e-4, epochs = 30, batch_size = 256"
 python train.py \
         --lr 1e-4 \
-        --epochs 20 \
+        --device 3 \
+        --epochs 30 \
         --batch_size 256 \
         --earlystop 0 \
-        --eval_iter 1 \
+        --eval_iter 30 \
         --model_folder 'models'  \
         --classifier CRED \
         --model_name CRED_1e4_256 \
         --dataset_name "STEAD-ZEROS" \
-        --train_path "Data/TrainReady/Train_constant.npy" \
-        --val_path "Data/TrainReady/Val_constant.npy"
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy"
 
 P5=$!
 wait $P1 $P2 $P3 $P4 $P5
 
-echo "Training model CRED, lr = 5e-5, epochs = 20, batch_size = 256"
+echo "Training model CRED, lr = 5e-5, epochs = 30, batch_size = 256"
 python train.py \
         --lr 5e-5 \
-        --epochs 20 \
+        --device 3 \
+        --epochs 30 \
         --batch_size 256 \
         --earlystop 0 \
-        --eval_iter 1 \
+        --eval_iter 30 \
         --model_folder 'models'  \
         --classifier CRED \
         --model_name CRED_5e5_256 \
         --dataset_name "STEAD-ZEROS" \
-        --train_path "Data/TrainReady/Train_constant.npy" \
-        --val_path "Data/TrainReady/Val_constant.npy" &
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy" &
 
 P1=$!
 
-echo "Training model CRED, lr = 1e-5, epochs = 20, batch_size = 256"
+echo "Training model CRED, lr = 1e-5, epochs = 30, batch_size = 256"
 python train.py \
         --lr 1e-5 \
-        --epochs 20 \
+        --device 3 \
+        --epochs 30 \
         --batch_size 256 \
         --earlystop 0 \
-        --eval_iter 1 \
+        --eval_iter 30 \
         --model_folder 'models'  \
         --classifier CRED \
         --model_name CRED_1e5_256 \
         --dataset_name "STEAD-ZEROS" \
-        --train_path "Data/TrainReady/Train_constant.npy" \
-        --val_path "Data/TrainReady/Val_constant.npy" &
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy" &
 
 P2=$!
 
-echo "Training model CRED, lr = 5e-6, epochs = 20, batch_size = 256"
+echo "Training model CRED, lr = 5e-6, epochs = 30, batch_size = 256"
 python train.py \
         --lr 5e-6 \
-        --epochs 20 \
+        --device 3 \
+        --epochs 30 \
         --batch_size 256 \
         --earlystop 0 \
-        --eval_iter 1 \
+        --eval_iter 30 \
         --model_folder 'models'  \
         --classifier CRED \
         --model_name CRED_5e6_256 \
         --dataset_name "STEAD-ZEROS" \
-        --train_path "Data/TrainReady/Train_constant.npy" \
-        --val_path "Data/TrainReady/Val_constant.npy" &
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy" &
 
 P3=$!
 
-echo "Training model CRED, lr = 1e-6, epochs = 20, batch_size = 256"
+echo "Training model CRED, lr = 1e-6, epochs = 30, batch_size = 256"
 python train.py \
         --lr 1e-6 \
-        --epochs 20 \
+        --device 3 \
+        --epochs 30 \
         --batch_size 256 \
         --earlystop 0 \
-        --eval_iter 1 \
+        --eval_iter 30 \
         --model_folder 'models'  \
         --classifier CRED \
         --model_name CRED_1e6_256 \
         --dataset_name "STEAD-ZEROS" \
-        --train_path "Data/TrainReady/Train_constant.npy" \
-        --val_path "Data/TrainReady/Val_constant.npy"
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy"
 
 P4=$!
 wait $P1 $P2 $P3 $P4
